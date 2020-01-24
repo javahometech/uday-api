3cfbd8609eb3fe407f6b541460326db1e5b81d8f
Hari Kammana <hari.kammana@hp.com>
2020-01-24 08:34:26 +0530

    first commit

---
+++ src/main/java/com/example/demo/EC2Controller.java
@@ -0,0 +1,32 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.service.EC2JsonService;
+
+@RestController
+public class EC2Controller {
+	@GetMapping("/instances")
+	public String instances() {
+		try {
+			return EC2JsonService.getInstances();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return "One Instance";
+	}
+	
+	@GetMapping("/instance/{id}")
+	public Object instance(@PathVariable(name = "id") String id) {
+		try {
+			return EC2JsonService.getInstance(id);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return "One Instance";
+	}
+}
---
+++ src/main/java/com/example/demo/service/EC2JsonService.java
@@ -0,0 +1,56 @@
+package com.example.demo.service;
+
+import java.io.BufferedInputStream;
+import java.io.InputStream;
+import java.net.URL;
+
+import com.google.common.io.Resources;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+public class EC2JsonService {
+
+	public static String getInstances() throws Exception {
+		URL resource = Resources.getResource("instances.json");
+		InputStream fis = resource.openStream();
+		BufferedInputStream bis = new BufferedInputStream(fis);
+		StringBuffer sb = new StringBuffer();
+		int i;
+		while ((i = bis.read()) != -1) {
+			sb.append((char) i);
+		}
+//		System.out.println(sb.toString());
+		JsonObject jsonObject = new JsonParser().parse(sb.toString()).getAsJsonObject();
+		
+		return jsonObject.toString();
+	}
+
+	public static String getInstance(String instanceId) throws Exception {
+		URL resource = Resources.getResource("instances.json");
+		InputStream fis = resource.openStream();
+		BufferedInputStream bis = new BufferedInputStream(fis);
+		StringBuffer sb = new StringBuffer();
+		int i;
+		while ((i = bis.read()) != -1) {
+			sb.append((char) i);
+		}
+		JsonObject jsonObject = new JsonParser().parse(sb.toString()).getAsJsonObject();
+		JsonArray asJsonArray = jsonObject.get("Reservations").getAsJsonArray();
+		for (JsonElement jsonElement : asJsonArray) {
+			JsonArray asJsonArray2 = jsonElement.getAsJsonObject().get("Instances").getAsJsonArray();
+			for (JsonElement jsonElement2 : asJsonArray2) {
+				JsonElement instance = jsonElement2.getAsJsonObject().get("InstanceId");
+				
+				if(instance.getAsString().equals(instanceId)) {
+					return jsonElement2.toString();
+				}
+				
+			}
+			
+		}
+		return "Instance Not found";
+	}
+
+}
